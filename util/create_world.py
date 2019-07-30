953d1dbcb8e713a2004629a44a52220507809751
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-31 02:15:45 +0530

    corrected spacing in create_world.py

--- util/create_world.py
+++ util/create_world.py
@@ -5,7 +5,7 @@ from adventure.models import Player, Room
 Room.objects.all().delete()
 
 r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+              description="North of you, the cave mount beckons")
 
 r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
 passages run north and east.""")
