dfce6c90b04c6e345d0d86d5a37b8e191aedb138
Anubhav311 <anubhavbhambri1@gmail.com>
2019-08-01 06:50:06 +0530

    bringing branch forward

--- adventure/api.py
+++ adventure/api.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from .models import *
 from rest_framework.decorators import api_view
 import json
+from adventure.models import Player, Room
 
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
