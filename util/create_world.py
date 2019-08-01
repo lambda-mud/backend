ad3c733c7e160da9231d4bb75bba4956f68eb9da
benjchand <benchand@gmail.com>
2019-08-01 11:44:47 -0700

    City descriptions are done.

--- util/create_world.py
+++ util/create_world.py
@@ -5,35 +5,35 @@ Room.objects.all().delete()
 
 roomsArray = [
     
-    {'title': 'Seattle', 'description': 'EmptyDescription'},
-    {'title': 'Spokane', 'description': 'EmptyDescription'},
-    {'title': 'Portland', 'description': 'EmptyDescription'},
-    {'title': 'Sacramento', 'description': 'EmptyDescription'},
-    {'title': 'San Francisco', 'description': 'EmptyDescription'},
-    {'title': 'Reno', 'description': 'EmptyDescription'},
-    {'title': 'Las Vegas', 'description': 'EmptyDescription'},
-    {'title': 'Phoenix', 'description': 'EmptyDescription'},
-    {'title': 'Boise', 'description': 'EmptyDescription'},
-    {'title': 'Salt Lake City', 'description': 'EmptyDescription'},
-    {'title': 'Helena', 'description': 'EmptyDescription'},
-    {'title': 'Billings', 'description': 'EmptyDescription'},
-    {'title': 'Yellowstone National Park', 'description': 'EmptyDescription'},
-    {'title': 'Cheyenne', 'description': 'EmptyDescription'},
-    {'title': 'Denver', 'description': 'EmptyDescription'},
-    {'title': 'Albuquerque', 'description': 'EmptyDescription'},
+    {'title': 'Seattle', 'description': 'The Emerald City.'},
+    {'title': 'Spokane', 'description': 'Creative by Nature'},
+    {'title': 'Portland', 'description': 'The City That Works'},
+    {'title': 'Sacramento', 'description': 'Almond Capital of the World'},
+    {'title': 'San Francisco', 'description': 'The City by the Bay'},
+    {'title': 'Reno', 'description': 'Biggest Little City in the World'},
+    {'title': 'Las Vegas', 'description': 'What Happens in Vegas, Stays in Vegas.'},
+    {'title': 'Phoenix', 'description': "It's a DRY heat.  Very efficient."},
+    {'title': 'Boise', 'description': 'Energy Peril Success. Whatever that means.'},
+    {'title': 'Salt Lake City', 'description': 'The Beehive Valley'},
+    {'title': 'Helena', 'description': 'Queen City of the Rockies'},
+    {'title': 'Billings', 'description': 'Magic City'},
+    {'title': 'Yellowstone National Park', 'description': 'Wonderland'},
+    {'title': 'Cheyenne', 'description': 'Magic City of the Plains'},
+    {'title': 'Denver', 'description': 'Mile High City'},
+    {'title': 'Albuquerque', 'description': 'The Duke City.  The towels are oh so fluffy.'},
     # Good to here 15
-    {'title': 'Bismarck', 'description': 'EmptyDescription'},
-    {'title': 'Fargo', 'description': 'EmptyDescription'},
-    {'title': 'Pierre', 'description': 'EmptyDescription'},
-    {'title': 'Lincoln', 'description': 'EmptyDescription'},
-    {'title': 'Wichita', 'description': 'EmptyDescription'},
-    {'title': 'Oklahoma City', 'description': 'EmptyDescription'},
-    {'title': 'Austin', 'description': 'EmptyDescription'},
-    {'title': 'Kansas City', 'description': 'EmptyDescription'},
-    {'title': 'Houston', 'description': 'EmptyDescription'}
+    {'title': 'Bismarck', 'description': 'The Skyscraper on the Prairie'},
+    {'title': 'Fargo', 'description': 'Gateway to the West'},
+    {'title': 'Pierre', 'description': 'There is nothing interesting about this city.'},
+    {'title': 'Lincoln', 'description': 'Husker City'},
+    {'title': 'Wichita', 'description': 'Air Capital of the World. Doo-Dah'},
+    {'title': 'Oklahoma City', 'description': 'The Big Friendly.  Sooners abound.'},
+    {'title': 'Austin', 'description': 'Keep Austin Weird'},
+    {'title': 'Kansas City', 'description': 'Paris of the Plains. Big jazz/barbecue place, apparently.'},
+    {'title': 'Houston', 'description': 'H-Town, Bayou City, The Big H.'}
 ]
 
-connectedCitiesIndex = [[0, 1, "e"], [ 0, 1, "s"], [ 1, 0, "w"], [ 1, 10, "e"], [ 1, 8, "s"], [ 2, 0, "n"], [ 2, 3, "s"], [ 3, 2, "n"], [ 3, 5, "e"], [ 3, 4, "s"], [ 4, 3, "n"], [ 4, 6, "e"], [ 4, 7, "s"], [ 5, 3, "w"], [ 5, 9, "e"], [ 6, 4, "w"], [ 6, 9, "e"], [ 7, 4, "w"], [ 7, 15, "e"], [ 8, 2, "n"], [ 8, 12, "e"], [ 8, 9, "s"], [ 9, 5, "w"], [ 9, 8, "n"], [ 9, 6, "s"], [ 9, 14, "e"], [ 10, 1, "w"], [ 10, 12, "s"], [ 10, 11, "e"], [ 11, 10, "w"], [ 11, 16, "e"], [ 12, 8, "w"], [ 12, 10, "n"], [ 12, 13, "e"], [ 13, 12, "w"], [ 13, 14, "s"], [ 14, 13, "n"], [ 14, 9, "w"], [ 14, 15, "s"], [ 14, 20, "e"], [ 15, 7, "w"], [ 15, 14, "n"], [ 16, 11, "w"], [ 16, 18, "s"], [ 16, 17, "e"], [ 17, 16, "w"], [ 18, 16, "n"], [ 18, 19, "s"], [ 19, 18, "n"], [ 19, 20, "s"], [ 20, 14, "w"], [ 20, 19, "n"], [ 20, 23, "e"], [ 20, 21, "s"], [ 21, 20, "n"], [ 21, 22, "s"], [ 22, 21, "n"], [ 22, 24, "e"], [ 23, 20, "w"], [ 24, 22, "e"]]
+connectedCitiesIndex = [[0, 1, "e"], [ 0, 1, "s"], [ 1, 0, "w"], [ 1, 10, "e"], [ 1, 8, "s"], [ 2, 0, "n"], [ 2, 3, "s"], [ 3, 2, "n"], [ 3, 5, "e"], [ 3, 4, "s"], [ 4, 3, "n"], [ 4, 6, "e"], [ 4, 7, "s"], [ 5, 3, "w"], [ 5, 9, "e"], [ 6, 4, "w"], [ 6, 9, "e"], [ 7, 4, "w"], [ 7, 15, "e"], [ 8, 2, "n"], [ 8, 12, "e"], [ 8, 9, "s"], [ 9, 5, "w"], [ 9, 8, "n"], [ 9, 6, "s"], [ 9, 14, "e"], [ 10, 1, "w"], [ 10, 12, "s"], [ 10, 11, "e"], [ 11, 10, "w"], [ 11, 16, "e"], [ 12, 8, "w"], [ 12, 10, "n"], [ 12, 13, "e"], [ 13, 12, "w"], [ 13, 14, "s"], [ 14, 13, "n"], [ 14, 9, "w"], [ 14, 15, "s"], [ 14, 20, "e"], [ 15, 7, "w"], [ 15, 14, "n"], [ 15, 22, "s"], [ 16, 11, "w"], [ 16, 18, "s"], [ 16, 17, "e"], [ 17, 16, "w"], [ 18, 16, "n"], [ 18, 19, "s"], [ 19, 18, "n"], [ 19, 20, "s"], [ 20, 14, "w"], [ 20, 19, "n"], [ 20, 23, "e"], [ 20, 21, "s"], [ 21, 20, "n"], [ 21, 22, "s"], [ 22, 21, "n"], [ 22, 24, "e"], [ 22, 15, "w"], [ 23, 20, "w"], [ 24, 22, "e"]]
 
 createdRooms = []
 
